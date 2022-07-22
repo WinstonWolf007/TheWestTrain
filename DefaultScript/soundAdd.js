c431d4a3f8b900bdf4d6ceca07825bc09c5bf5e9
WinstonWolf007 <ubguertin@gmail.com>
2022-07-22 16:15:14 -0400

    update combat system

--- DefaultScript/soundAdd.js
+++ DefaultScript/soundAdd.js
@@ -1,7 +1,12 @@
 // just a shotcut for add sound
-function SoundAdd(self, name, vol, loop) {
-    return self.sound.add(name, {
-        volume: vol,
-        loop: loop
-    })
+function SoundAdd(self, name, loop) {
+    let type = name.split(':')[0]
+    let struct = {volume: 0, loop: loop}
+    if (type == 'sound') {
+        struct['volume'] = JSON['volume']['fg']
+    }
+    else if (type == 'music') {
+        struct['volume'] = JSON['volume']['bg']
+    }
+    return self.sound.add(name, struct)
 }
--- Scenes/battle/battle.js
+++ Scenes/battle/battle.js
@@ -3,47 +3,98 @@ class Battle extends Phaser.Scene {
         super('battle')
     }
 
+    chrono() {
+        // ----- Tick Sound ----- //
+        this.s321 = SoundAdd(this, 'sound:321', false);
+        this.sGo = SoundAdd(this, 'sound:go', false)
+
+        // ----- Time Txt ----- //
+        this.timeTxt = this.add.text((1500-100)/2, 200, '', {fontSize: 100, fontFamily: 'pixelMoney'});
+
+        this.timeTxt.text = "3"
+        this.timeTxt.setTint(0xff0000)
+        this.s321.play()
+        setTimeout(() => {
+            this.timeTxt.text = "2"
+            this.timeTxt.setTint(0xff0000)
+            this.s321.play()
+            setTimeout(() => {
+                this.timeTxt.text = "1"
+                this.timeTxt.setTint(0xff0000)
+                this.s321.play()
+                setTimeout(() => {
+                    this.startShootGame = true
+                    this.timeTxt.text = ">"
+                    this.timeTxt.setTint(0x00ff00)
+                    this.sGo.play()
+                    setTimeout(() => {
+                        this.timeTxt.text = ""
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }
+
     preload() {
         this.cameras.main.fadeIn(1000, 0, 0, 0);
     }
 
     create() {
-        // ----- Music + Sound ----- //
-        this.music1 = SoundAdd(this, 'music:aspire', JSON['volume']['bg'], true)
-        this.music1.play()
+        // ----- game load loop ----- //
+        this.gunLoad = true;
+        this.enterLoad = true;
 
-        this.gunShoot2 = SoundAdd(this, 'sound:gunShoot2', JSON['volume']['fg'], false);
+        this.intervalGunLoad = setInterval(() => {
+            this.gunLoad = true; 
+            this.enterLoad = true;
+        }, 1000)
+
+        // ----- Game ----- //
+        this.gameLoad = false
+
+        // ----- Music + Sound ----- //
+        SoundAdd(this, 'music:irreducible', true).play();
+
+        this.gunShoot2  = SoundAdd(this, 'sound:gunShoot2', false);
+        this.deathSound = SoundAdd(this, 'sound:electricity', false);
 
         // ----- Image ----- //
-        this.map = this.physics.add.sprite(0, 0, 'spritesheet:battleMap').setOrigin(0, 0);
-        
-        this.enemy = this.physics.add.sprite(1200, 700, 'spritesheet:battleEntity', 0)
-        this.enemy.flipX = true;
+        this.map          = this.physics.add.sprite(0, 0, 'spritesheet:battleMap').setOrigin(0, 0);
 
-        this.player = this.physics.add.sprite(300, 700, 'spritesheet:battleEntity', 3).setActive(false).setVisible(false)
-        
-        this.deathE = this.physics.add.sprite(1200, 700, 'spritesheet:deathEntity', 0).setActive(false).setVisible(false)
+        this.enemy        = this.physics.add.sprite(1200, 700, 'spritesheet:battleEntity', 0).setActive(false).setVisible(false);
+        this.deathE       = this.physics.add.sprite(1200, 700, 'spritesheet:deathEntity', 0).setActive(false).setVisible(false);
+        this.iddleE       = this.physics.add.sprite(1200, 700, 'spritesheet:iddleEntity', 0);
+        this.enemy.flipX  = true;
+        this.iddleE.flipX = true;
+        this.deathE.flipX = true;
 
-        this.iddleP = this.physics.add.sprite(300, 700, 'spritesheet:iddleEntity', 2);
-        
+        this.player       = this.physics.add.sprite(300, 700, 'spritesheet:battleEntity', 3).setActive(false).setVisible(false);
+        this.iddleP       = this.physics.add.sprite(300, 700, 'spritesheet:iddleEntity', 2);
+        this.deathP       = this.physics.add.sprite(300, 700, 'spritesheet:deathEntity', 7).setActive(false).setVisible(false);
+
         // ----- Health bar player and enemy ----- //
-        let x1 = 140
-        let x2 = 1040
-        let y = 400
-        let bgHealthPlayer = this.add.graphics();
-        let fgHealthPlayer = this.add.graphics();
-        let bgHealthEnemy = this.add.graphics();
-        let fgHealthEnemy = this.add.graphics();
+        this.x1             = 140;
+        this.x2             = 1040;
+        this.y              = 400;
+        this.bgHealthPlayer = this.add.graphics();
+        this.fgHealthPlayer = this.add.graphics();
+        this.bgHealthEnemy  = this.add.graphics();
+        this.fgHealthEnemy  = this.add.graphics();
+        this.Edeath        = false;
+        this.Pdeath        = false;
         
-        bgHealthPlayer.fillStyle(0x222222, 0.8);
-        bgHealthPlayer.fillRect(x1, y, 300, 30);
-        fgHealthPlayer.fillStyle(0x4ADBD1, 0.8);
-        fgHealthPlayer.fillRect(x1, y, 300, 30);
-
-        bgHealthEnemy.fillStyle(0x222222, 0.8);
-        bgHealthEnemy.fillRect(x2, y, 300, 30);
-        fgHealthEnemy.fillStyle(0x4ADBD1, 0.8);
-        fgHealthEnemy.fillRect(x2, y, 300, 30);
+        this.bgHealthPlayer.fillStyle(0x222222, 0.8);
+        this.bgHealthPlayer.fillRect(this.x1, this.y, 300, 30);
+        this.fgHealthPlayer.fillStyle(0x4ADBD1, 0.8);
+        this.fgHealthPlayer.fillRect(this.x1, this.y, 300, 30);
+        
+        this.bgHealthEnemy.fillStyle(0x222222, 0.8);
+        this.bgHealthEnemy.fillRect(this.x2, this.y, 300, 30);
+        this.fgHealthEnemy.fillStyle(0x4ADBD1, 0.8);
+        this.fgHealthEnemy.fillRect(this.x2, this.y, 300, 30);
+        
+        // ----- Shoot Game ----- //
+        this.startShootGame = false;
 
         // ----- Animation ----- //
         this.anims.create({
@@ -61,46 +112,83 @@ class Battle extends Phaser.Scene {
         });
 
         this.anims.create({
+            key: 'deathP',
+            frames: this.anims.generateFrameNumbers('spritesheet:deathEntity', { start: 7, end: 14 }),
+            frameRate: 10,
+            repeat: 0
+        });
+
+        this.anims.create({
             key: 'iddleP',
             frames: this.anims.generateFrameNumbers('spritesheet:iddleEntity', { start: 2, end: 3 }),
             frameRate: 1,
             repeat: -1
         })
 
-        this.map.anims.play('iddleBG');
-        this.iddleP.play('iddleP')
-        
-        // ----- Key board Event ----- //
-        this.input.keyboard.on('keydown-SPACE', () => {
-            this.gunShoot2.play()
-            enemyHealth -= 20;
-            this.player.setActive(true).setVisible(true)
-            this.iddleP.setActive(false).setVisible(false)
-            fgHealthEnemy.clear();
-            fgHealthEnemy.fillStyle(0x4ADBD1, 0.8);
-            fgHealthEnemy.fillRect(x2, y, enemyHealth*3, 30);
-            if (enemyHealth < 1) {
-                this.enemy.setActive(false).setVisible(false);
-                this.deathE.setActive(true).setVisible(true);
-                this.deathE.flipX = true;
-                this.deathE.anims.play('deathE');
-
-                // setTimeout(() => {
-                //     this.cameras.main.fadeOut(1000, 0, 0, 0);
-                // }, 1000)
-                
-                // setTimeout(() => {
-                //     this.scene.start('home')
-                //     this.scene.stop('battle')
-                // }, 1000)
-            }
-
-            setTimeout(() => {      
-                setTimeout(() => {
-                    this.player.setActive(false).setVisible(false)
-                    this.iddleP.setActive(true).setVisible(true)
-                }, 100)
-            }, 500)
+        this.anims.create({
+            key: 'iddleE',
+            frames: this.anims.generateFrameNumbers('spritesheet:iddleEntity', { start: 0, end: 1 }),
+            frameRate: 1,
+            repeat: -1
         })
+
+        this.map.anims.play('iddleBG');
+        this.iddleP.play('iddleP');
+        this.iddleE.play('iddleE');
+
+        // ----- title ----- //
+        this.infoTxt = this.add.text(380, 200, 'Press enter button', {fontSize: 50, fontFamily: 'pixelMoney'});
+
+        this.input.keyboard.on('keydown-ENTER', () => {
+            if (!this.startShootGame) {
+                this.infoTxt.text = "";
+                this.chrono()
+                this.gameLoad = true;
+            }
+        })
+    }
+
+    ShootGame() {
+        this.input.keyboard.on('keydown-SPACE', () => {
+            if (!this.Edeath && this.gunLoad && this.startShootGame) {
+                this.gunShoot2.play();
+
+                enemyHealth -= 20;
+
+                this.fgHealthEnemy.clear();
+                this.fgHealthEnemy.fillStyle(0x4ADBD1, 0.8);
+                this.fgHealthEnemy.fillRect(this.x2, this.y, enemyHealth*3, 30);
+
+                this.iddleP.setActive(false).setVisible(false);
+                this.player.setActive(true).setVisible(true);
+                
+
+                if (enemyHealth < 1) {
+                    this.deathSound.play()
+
+                    this.iddleE.setActive(false).setVisible(false);
+                    this.deathE.setActive(true).setVisible(true);
+
+                    this.deathE.anims.play('deathE');
+
+                    this.Edeath = true;
+                }
+                this.gunLoad = false;
+                this.startShootGame = false;
+
+                setTimeout(() => {      
+                    this.player.setActive(false).setVisible(false);
+                    this.iddleP.setActive(true).setVisible(true);
+                }, 500)
+            } 
+            else if (!this.startShootGame && this.enterLoad && this.gameLoad) {
+                alert('tres rapide xD !!')
+                this.enterLoad = false;
+            }
+        })
+    }
+
+    update() {
+        this.ShootGame()
     }
 }
